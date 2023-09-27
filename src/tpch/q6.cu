5e8edc18ce5de45dce3715fbfa03667d746b964a
Devesh Pankaj Sarda <devess@cs.washington.edu>
2023-09-27 15:30:37 -0700

    Added multi trial support

--- src/tpch/q6.cu
+++ src/tpch/q6.cu
@@ -6,6 +6,7 @@
 #include <curand.h>
 #include <string>
 #include <cuda.h>
+#include <cstdlib>
 #include <cub/util_allocator.cuh>
 #include "cub/test/test_util.h"
 
@@ -19,6 +20,7 @@ using std::chrono::high_resolution_clock;
 using std::chrono::duration;
 
 #define NUM_BLOCKS 640
+#define NUM_TRIALS 10
 
 /**
  * Globals, constants and typedefs
@@ -45,8 +47,8 @@ float* d_l_extendedprice, float* total, int lo_num_entries) {
     }
 }
 
-float runQuery(int* d_l_shipdate, float* d_l_discount, float* d_l_quantity, 
-float* d_l_extendedprice, int lo_num_entries, cub::CachingDeviceAllocator&  g_allocator) {
+float runQuery(int* d_l_shipdate, float* d_l_discount, float* d_l_quantity, float* d_l_extendedprice, 
+int lo_num_entries, cub::CachingDeviceAllocator&  g_allocator, float* results, float* timings, int trial_num) {
     // Setup all the timing stuff
     cudaEvent_t gpu_start, gpu_stop; 
     cudaEventCreate(&gpu_start); 
@@ -81,13 +83,38 @@ float* d_l_extendedprice, int lo_num_entries, cub::CachingDeviceAllocator&  g_al
     duration_ms = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
 
     // Log the results
-    std::cout << "Revenue: " << revenue << std::endl;
-    std::cout << "Time taken GPU: " << time_query << " ms" << std::endl;
-    std::cout << "Time taken total: " << duration_ms.count() << " ms" << std::endl;
+    results[trial_num] = revenue;
+    timings[2 * trial_num] = time_query;
+    timings[2 * trial_num + 1] = (float) duration_ms.count();
 
     CubDebugExit(g_allocator.DeviceFree(total));
     return time_query;
 }
+
+#define MAX_ALLOWED_DIFF 500
+void verifySameResult(float* results, int num_trials) {
+  float expected = results[0];
+  for(int i = 1; i < num_trials; i++) {
+    float diff = std::abs(expected - results[i]);
+    if(diff > MAX_ALLOWED_DIFF) {
+      std::cerr << "Idx " << i << " has value " << results[i] << " but idx 0 has value " << expected 
+      << ", which have a diff of " << diff << std::endl;
+      exit(1);
+    }
+  }
+}
+
+void printTimings(float* timings, int num_trials) {
+  float gpuTotalTime = 0.0;
+  float overallTotalTime = 0.0;
+  for(int i = 0; i < num_trials; i += 2) {
+    gpuTotalTime += timings[i];
+    overallTotalTime += timings[i + 1];
+  }
+  std::cout << "Average time taken GPU: " << gpuTotalTime/num_trials << std::endl;
+  std::cout << "Average time taken overall: " << overallTotalTime/num_trials << std::endl;
+} 
+
 /**
  * Main
  */
@@ -112,8 +139,17 @@ int main(int argc, char** argv)
   float *d_l_quantity = loadToGPU<float>(h_l_quantity, LO_LEN, g_allocator);
   float *d_l_extendedprice = loadToGPU<float>(h_l_extendedprice, LO_LEN, g_allocator);
 
-  // Call the kernel
-  runQuery(d_l_shipdate, d_l_discount, d_l_quantity, d_l_extendedprice, LO_LEN, g_allocator);
+  // Perform multiple trials
+  float* results = new float[NUM_TRIALS];
+  float* timings = new float[2 * NUM_TRIALS];
+  for(int trial_num = 0; trial_num < NUM_TRIALS; trial_num++) {
+    runQuery(d_l_shipdate, d_l_discount, d_l_quantity, d_l_extendedprice, 
+    LO_LEN, g_allocator, results, timings, trial_num);
+  }
+
+  // Output the timings
+  verifySameResult(results, NUM_TRIALS);
+  printTimings(timings, NUM_TRIALS);
 
   // Free the memory
   CubDebugExit(cudaFree(d_l_shipdate));
